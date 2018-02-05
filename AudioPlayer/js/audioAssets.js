8b599bf34318eb26722113864a5620a4e18dba25
yasmeen789 <yasmeen.hazy@gmail.com>
2018-02-05 21:48:24 +0000

    Outputs the story in sections.

--- AudioPlayer/js/audioAssets.js
+++ AudioPlayer/js/audioAssets.js
@@ -2,12 +2,28 @@
 
 // Audio Source - AWS Podcast : https://aws.amazon.com/podcasts/aws-podcast/
 var audioData = [{
-    'title': 'The Gruffalo by Julia Donaldson',
-    'url': 'https://s3-eu-west-1.amazonaws.com/yasmeenbucket/The+Gruffalo.mp3'
+    'title': 'Part 1',
+    'url': 'https://s3-eu-west-1.amazonaws.com/yasmeenbucket/The_Gruffalo_part1.mp3'
   },
   {
-    'title': 'Episode 140',
-    'url': 'https://feeds.soundcloud.com/stream/275202399-amazon-web-services-306355661-amazon-web-services.mp3'
+    'title': 'Part 2',
+    'url': 'https://s3-eu-west-1.amazonaws.com/yasmeenbucket/The_Gruffalo_part2.mp3'
+  },
+  {
+    'title': 'Part 3',
+    'url': 'https://s3-eu-west-1.amazonaws.com/yasmeenbucket/The_Gruffalo_part3.mp3'
+  },
+  {
+    'title': 'Part 4',
+    'url': 'https://s3-eu-west-1.amazonaws.com/yasmeenbucket/The_Gruffalo_part4.mp3'
+  },
+  {
+    'title': 'Part 5',
+    'url': 'https://s3-eu-west-1.amazonaws.com/yasmeenbucket/The_Gruffalo_part5.mp3'
+  },
+  {
+    'title': 'Part 6',
+    'url': 'https://s3-eu-west-1.amazonaws.com/yasmeenbucket/The_Gruffalo_part6.mp3'
   }
 ];
 
